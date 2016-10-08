de1ea8490891253c56a6b2e1a856a27aac8e7728
Andrew Munro <andrew.munro@skybettingandgaming.com>
2016-10-08 19:18:57 +0100

    Adding fullscreen and fixing some css.

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,31 @@ export default class Application extends React.Component {
         super(props, context);
     }
 
+    toggleFullscreen() {
+        if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+            (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+            if (document.documentElement.requestFullScreen) {
+                document.documentElement.requestFullScreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullScreen) {
+                document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else {
+            if (document.cancelFullScreen) {
+                document.cancelFullScreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            }
+        }
+    }
+
     render() {
         return (
             <div>
-                <Nav />
+                <button className='toggle-fullscreen-btn' onClick={() => this.toggleFullscreen()} ></button>
                 {this.props.children}
             </div>
         );
