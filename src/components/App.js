cac79a1b80145e70e6c7b4f3da14195bd674849d
Andrew Munro <andrew.munro@skybettingandgaming.com>
2016-10-18 22:21:38 +0100

    Adding mute button

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Nav from 'components/Nav';
+import Bamboo from 'game/bamboo/Bamboo';
 
 export default class Application extends React.Component {
     constructor(props, context) {
         super(props, context);
+
+        this.muted = false;
+
+        this.state = {
+            muteImage: '/images/mute-button.svg'
+        }
     }
 
     toggleFullscreen(event) {
@@ -29,10 +36,29 @@ export default class Application extends React.Component {
         event.target.blur();
     }
 
+    toggleMute(event) {
+        if(this.muted) {
+            Bamboo.instance.soundManager.soundManager.unmute();
+        } else {
+            Bamboo.instance.soundManager.soundManager.mute();
+        }
+
+        this.setState({
+            muteImage: this.muted ? '/images/mute-button.svg' : '/images/unmute-button.svg'
+        });
+
+        this.muted = !this.muted;
+
+        event.target.blur();
+    }
+
     render() {
         return (
             <div>
                 <button className='toggle-fullscreen-btn' onClick={(event) => this.toggleFullscreen(event)} ></button>
+                <button className='toggle-sound-btn' onClick={(event) => this.toggleMute(event)} >
+                    <image src={this.state.muteImage}/>
+                </button>
                 {this.props.children}
             </div>
         );
